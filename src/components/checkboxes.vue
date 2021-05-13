dbaba2517bcabb9d47b7595b8b6973dda2e1a7bf
david <Monday - Friday 9-Noon Just give me a call!>
2021-05-12 18:14:36 -0700

    added a couple components

---
+++ src/components/checkboxes.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <input type="checkbox" id="jack" value="Jack" v-model="checkedValues" />
+    <label for="jack">Jack</label>
+  </div>
+</template>
+
+<script>
+import data from '../assets/data.json'
+export default {
+  name: "app",
+  data: data,
+  checkedValues: {}
+};
+</script>
+
+
+<style lang="scss">
+
+
+</style>
